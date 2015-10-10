05f9e26f615866a780ec0997f5dc175857a12b25
Suraj <sundereshwaran@gmail.com>
2015-10-10 21:02:42 +0530

    Added basic term aggregations

--- EST/app/js/custom/MainController.js
+++ EST/app/js/custom/MainController.js
@@ -1,12 +1,13 @@
 var gs;
-app.controller('MainController', ['$scope', '$http', 'ESService', function($scope, $http, $ess) {
+app.controller('MainController', ['$scope', '$http', 'ESService', function ($scope, $http, $ess) {
     $scope.Title = "elasticsearch toolbox";
     $scope.qfieldTemp = [];
+    $scope.qAggrTemp = [];
     gs = $scope;
-   
-    $scope.connectToES = function(url) {
+
+    $scope.connectToES = function (url) {
         Materialize.toast('Connecting to elasticsearch', 500);
-        $ess.getIndexes(url).then(function(stat) {
+        $ess.getIndexes(url).then(function (stat) {
             Materialize.toast('Connected to elasticsearch', 3000, 'green');
             $scope.indexes = Object.keys(stat.data.indices);
             $scope.esStat = stat.data;
@@ -14,11 +15,11 @@ app.controller('MainController', ['$scope', '$http', 'ESService', function($scop
         });
     };
 
-    $scope.changeIndex = function(es) {
+    $scope.changeIndex = function (es) {
         $scope.indexInfo = $scope.indexes[es.selectedIndex];
         var url = String.format("{0}/{1}", es.url, es.selectedIndex);
         $ess.getMappings(url)
-            .then(function(response) {
+            .then(function (response) {
                 //console.log("Mappings", response);
                 $scope.Mappings = response;
 
@@ -26,13 +27,13 @@ app.controller('MainController', ['$scope', '$http', 'ESService', function($scop
                 var mappingsObj = (response[Object.keys(response)]).mappings;
                 $scope.MappingList = Object.keys(mappingsObj);
 
-                $.each($scope.MappingList, function(k, v) {
+                $.each($scope.MappingList, function (k, v) {
                     $scope.allMappings = $scope.allMappings.concat(Object.keys(mappingsObj[v].properties));
                 });
             });
 
     };
-    $scope.changeMapping = function(es) {
+    $scope.changeMapping = function (es) {
 
         var props = Object.keys($scope.Mappings[es.selectedIndex].mappings[es.selectedMapping].properties);
         $scope.allMappings = props;
@@ -50,7 +51,7 @@ app.controller('MainController', ['$scope', '$http', 'ESService', function($scop
         queryEditor.autoFormatRange(range.from, range.to);
         CodeMirror.commands["singleSelection"](queryEditor);
     };
-    $scope.addToQuery = function(esQ) {
+    $scope.addToQuery = function (esQ) {
         if (esQ == undefined || Object.keys(esQ).length != 4) {
             Materialize.toast('You need to fill in all the fields', 3000, 'orange darken-4');
         } else {
@@ -64,7 +65,22 @@ app.controller('MainController', ['$scope', '$http', 'ESService', function($scop
 
 
     };
-    $scope.execute = function(es) {
+    $scope.addAggrToQuery = function (esQA) {
+
+        if (esQA == undefined || Object.keys(esQA).length != 3) {
+            Materialize.toast('You need to fill in all the fields to add it to the query', 3000, 'orange darken-4');
+
+        } else {
+            if (esQA.name.length > 0 && esQA.field.length > 0 && esQA.type.length > 0) {
+                $scope.qAggrTemp.push(esQA);
+                $scope.esQA = {};
+            } else {
+                Materialize.toast('You need to fill in all the fields', 3000, 'orange darken-4');
+            }
+        }
+
+    };
+    $scope.execute = function (es) {
         if (!es.selectedIndex) {
             Materialize.toast('Select an index', 3000, 'orange');
             $('#index').removeClass('shake').removeClass('animated').addClass('shake').addClass('animated');
@@ -73,20 +89,19 @@ app.controller('MainController', ['$scope', '$http', 'ESService', function($scop
 
         var queryString = '';
         var queryObj = '';
-        if ($scope.settings.useEditor)
-        {
+        if ($scope.settings.useEditor) {
             queryString = queryEditor.getValue();
         } else {
-            var queryObj = BuildQuery($scope.qfieldTemp)
+            var queryObj = BuildQuery($scope.qfieldTemp, $scope.qAggrTemp)
             queryString = JSON.stringify(queryObj)
         }
-       
+
         console.debug(queryString);
 
         var url = String.format("{0}/{1}/_search", es.url, es.selectedIndex);
 
         $ess.executeQuery(url, queryString)
-            .then(function(response) {
+            .then(function (response) {
                 //console.log("Q Result", response);
                 $scope.Output = response;
                 queryEditor.setValue(queryString);
@@ -100,7 +115,7 @@ app.controller('MainController', ['$scope', '$http', 'ESService', function($scop
             });
     };
 
-    $scope.saveSettings = function(settings) {
+    $scope.saveSettings = function (settings) {
         resultEditor.setOption("theme", settings.theme);
         queryEditor.setOption("theme", settings.theme);
         setTimeout(function () {
@@ -110,19 +125,19 @@ app.controller('MainController', ['$scope', '$http', 'ESService', function($scop
         if (settings)
             chrome.storage.sync.set({
                 'settings': settings
-            }, function() {
+            }, function () {
                 Materialize.toast('Settings saved', 3000, 'green');
             });
     };
 }]);
 
-function BuildQuery(queryParams) {
+function BuildQuery(queryParams,aggParams) {
     var queryTemplate = $.parseJSON(queryEditor.getValue());
 
     //Adding selected fields
     var jQfields = $('.fields:checked');
     var fields = [];
-    $.each(jQfields, function(k, field) {
+    $.each(jQfields, function (k, field) {
         fields.push($(field).prop('name'));
     });
     if (fields.length > 0)
@@ -130,6 +145,8 @@ function BuildQuery(queryParams) {
 
     //Adding Query Params
     queryTemplate.query.bool = GetQuery(queryParams);
+    queryTemplate.aggregations = GetAggQuery(aggParams);
+
     queryTemplate.from = gs.esQP.from;
     queryTemplate.size = gs.esQP.size;
 
@@ -141,7 +158,7 @@ function GetQuery(queryParams) {
     var mustList = [],
         mustNotList = [],
         shouldList = [];
-    $.each(queryParams, function(i, esQ) {
+    $.each(queryParams, function (i, esQ) {
 
         switch (esQ.condition) {
             case "must":
@@ -174,7 +191,14 @@ function GetQuery(queryParams) {
     };
 
 }
-
+function GetAggQuery(aggParams) {
+    var agg = [];
+    $.each(aggParams, function (i, esQA) {
+        agg.push(aggGenerator(esQA));
+    });
+    var AllAggTogether = agg.join();
+    return $.parseJSON('{'+AllAggTogether+'}');
+}
 function QueryObjGenerator(type, field, value) {
     var objStr = "";
     if (type == "query_string") {
@@ -186,4 +210,13 @@ function QueryObjGenerator(type, field, value) {
     var json = $.parseJSON(objStr);
     //console.log(type, json);
     return json;
+}
+function aggGenerator(esQA) {
+  
+   
+    var objStr = String.format('"{0}":{"{1}":{"field":"{2}"}}', esQA.name, esQA.type, esQA.field);
+   
+ //   var json = $.parseJSON(objStr);
+    //console.log(type, json);
+    return objStr;
 }
