86e411ac978cdd397000fce56508cb608697d976
Nicolas SAVOIS <nicolas.savois@gmail.com>
2016-09-02 12:22:49 +0200

    added shortcut to execute query when Ctrl+Enter is pressed

--- EST/js/app/Plugin.js
+++ EST/js/app/Plugin.js
@@ -56,6 +56,9 @@ function initCodeMirror(element) {
         extraKeys: {
             "Ctrl-Q": function(cm) {
                 cm.foldCode(cm.getCursor());
+            },
+            "Ctrl-Enter": function(cm) {
+                gs.execute(gs.es);
             }
         },
         foldGutter: true,
